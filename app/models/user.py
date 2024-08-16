b84f6703aae95fdcd337542c4aa13308d3fff4ee
adsferreira <adriano.unicamp@gmail.com>
2024-08-16 19:10:09 -0300

    Refactoring the model's 'User' class to include the e-mail, first name and last name properties.

--- app/models/user.py
+++ app/models/user.py
@@ -5,23 +5,28 @@ from app import db
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True, nullable=False)
-    password_hash = Column(String(128), nullable=False)
-    role = Column(String(50), nullable=False)  # e.g., 'user' or 'admin'
+    usrId = Column(db.Integer, primary_key=True)
+    usrEmail = db.Column(db.String(120), unique=True, nullable=False)
+    usrFirstName = db.Column(db.String(64), nullable=False)
+    usrLastName = db.Column(db.String(64), nullable=False)
+    usrPasswordHash = Column(db.String(128), nullable=False)
+    usrRole = Column(db.String(50), nullable=False)  # e.g., 'user' or 'admin'
 
     def set_password(self, password):
         """Hash and set the user's password."""
-        self.password_hash = generate_password_hash(password)
+        self.usrPasswordHash = generate_password_hash(password)
 
     def check_password(self, password):
         """Check the hashed password."""
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.usrPasswordHash, password)
 
     def as_dict(self):
         """Convert the User object to a dictionary."""
         return {
-            'id': self.id,
-            'username': self.username,
-            'role': self.role
+            'usrId': self.id,
+            'usrEmail': self.name,
+            'usrFirstName': self.usrFirstName,
+            'usrLastName': self.usrLastName,
+            'usrPasswordHash': self.usrPasswordHash,
+            'usrRole': self.usrRole
         }

46964501d994517b1230ef15da2d6157f9e107fe
adsferreira <adriano.unicamp@gmail.com>
2024-08-16 19:32:22 -0300

    Redefining the length of the string for the 'usrFirstName' and 'usrLastName' properties.

--- app/models/user.py
+++ app/models/user.py
@@ -6,11 +6,11 @@ class User(db.Model):
     __tablename__ = 'user'
 
     usrId = Column(db.Integer, primary_key=True)
-    usrEmail = db.Column(db.String(120), unique=True, nullable=False)
+    usrEmail = db.Column(db.String(128), unique=True, nullable=False)
     usrFirstName = db.Column(db.String(64), nullable=False)
     usrLastName = db.Column(db.String(64), nullable=False)
     usrPasswordHash = Column(db.String(128), nullable=False)
-    usrRole = Column(db.String(50), nullable=False)  # e.g., 'user' or 'admin'
+    usrRole = Column(db.String(32), nullable=False)  # e.g., 'user' or 'admin'
 
     def set_password(self, password):
         """Hash and set the user's password."""

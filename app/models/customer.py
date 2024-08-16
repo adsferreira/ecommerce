f5dc604941e4c5c121f58c4af26077597c0a82bb
adsferreira <adriano.unicamp@gmail.com>
2024-08-16 19:16:47 -0300

    Refactoring the model's 'Customer' class by removing the e-mail, first name and last name properties, and add a 'usrId' property representing a foreign key of the user table.

--- app/models/customer.py
+++ app/models/customer.py
@@ -6,9 +6,6 @@ class Customer(db.Model):
 
     Attributes:
         cusId (int): Primary key, unique identifier for each customer.
-        cusFirstName (str): The first name of the customer, cannot be null.
-        cusLastName (str): The last name of the customer, cannot be null.
-        cusEmail (str): The email address of the customer, cannot be null.
         cusAddress (str): The physical address of the customer, cannot be null.
         cusCity (str): The city of the customer's address, cannot be null.
         cusState (str): The state of the customer's address, cannot be null.
@@ -23,15 +20,14 @@ class Customer(db.Model):
     __tablename__ = 'customer'
 
     cusId = db.Column(db.Integer, primary_key=True)
-    cusFirstName = db.Column(db.String, nullable=False)
-    cusLastName = db.Column(db.String, nullable=False)
-    cusEmail = db.Column(db.String, nullable=False)
-    cusAddress = db.Column(db.String, nullable=False)
-    cusCity = db.Column(db.String, nullable=False)
-    cusState = db.Column(db.String, nullable=False)
-    cusCountry = db.Column(db.String, nullable=False)
-    cusPostalCode = db.Column(db.String, nullable=False)
-    cusPhoneNumber = db.Column(db.String)
+    cusAddress = db.Column(db.String(64), nullable=False)
+    cusCity = db.Column(db.String(32), nullable=False)
+    cusState = db.Column(db.String(32), nullable=False)
+    cusCountry = db.Column(db.String(32), nullable=False)
+    cusPostalCode = db.Column(db.String(16), nullable=False)
+    cusPhoneNumber = db.Column(db.String(16))
+    usrId = db.Column(db.Integer, db.ForeignKey('user.usrId'), nullable=False)
 
     # Establish relationship to CustomerOrder
     orders = db.relationship('CustomerOrder', backref='customer', lazy=True)
+    user = db.relationship('User', back_populates='customers')

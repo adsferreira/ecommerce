fa2f6794ce656907415e53ac53998daf07fda5dc
adsferreira <adriano.unicamp@gmail.com>
2024-08-16 14:46:24 -0300

    Including the authorization blueprint and JWT (JSON Web Token) for improve the security of transmitting information.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
-# Initialize SQLAlchemy
+# Initialize SQLAlchemy, Migrate, and JWTManager
 db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, to suppress a warning
+    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Set this to a secure random value
+
     db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
 
     with app.app_context():
         # Import models to ensure they're registered with SQLAlchemy
@@ -27,12 +35,14 @@ def create_app():
         from app.routes.product_routes import product_bp
         from app.routes.customer_order_routes import order_bp
         from app.routes.order_items_routes import order_items_bp
+        from app.auth_routes import auth_bp
 
         app.register_blueprint(customer_bp)
         app.register_blueprint(department_bp)
         app.register_blueprint(product_bp)
         app.register_blueprint(order_bp)
         app.register_blueprint(order_items_bp)
+        app.register_blueprint(auth_bp)
 
     @app.cli.command()
     def initdb():

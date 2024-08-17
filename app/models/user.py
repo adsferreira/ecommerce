92ffc8e2dcc8047dbdaa8cc642af091c14136988
adsferreira <adriano.unicamp@gmail.com>
2024-08-17 07:41:03 -0300

    Updating the documentation of the User model class.

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,21 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 class User(db.Model):
+    """
+        Represents the 'user' table in the database.
+
+        Attributes:
+            usrId (int): Primary key, unique identifier for each user.
+            usrEmail (str): Email address of the user, must be unique and cannot be null.
+            usrFirstName (str): User's first name, cannot be null.
+            usrLastName (str): User's last name, cannot be null.
+            usrPasswordHash (str): Hashed password of the user, cannot be null.
+            usrRole (str): Role of the user, e.g., 'user' or 'admin', cannot be null.
+
+        Relationships:
+            customer: One-to-One relationship with the Customer model.
+                      This allows retrieving the customer information associated with the user.
+        """
     __tablename__ = 'user'
 
     usrId = Column(db.Integer, primary_key=True)
@@ -15,15 +30,33 @@ class User(db.Model):
     customer = db.relationship('Customer', back_populates='user', uselist=False)
 
     def set_password(self, password):
-        """Hash and set the user's password."""
+        """
+                Hash and set the user's password.
+
+                Args:
+                    password (str): The password to hash and set.
+                """
         self.usrPasswordHash = generate_password_hash(password)
 
     def check_password(self, password):
-        """Check the hashed password."""
+        """
+               Check the hashed password against the provided password.
+
+               Args:
+                   password (str): The password to check.
+
+               Returns:
+                   bool: True if the password matches, False otherwise.
+               """
         return check_password_hash(self.usrPasswordHash, password)
 
     def as_dict(self):
-        """Convert the User object to a dictionary."""
+        """
+               Convert the User object to a dictionary.
+
+               Returns:
+                   dict: A dictionary representation of the User object.
+               """
         return {
             'usrId': self.usrId,
             'usrEmail': self.usrEmail,
